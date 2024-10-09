0be67d6bf86f2de357d135beac72d595a6d5ecec
jyoti <jyotiprakash.cse@gmail.com>
2024-10-09 12:28:24 +0530

    Updates

---
+++ precache-manifest.e491922b83d716377df0a957fd0cd462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4e5ccd7f6c344654f42ee9038cd007",
+    "url": "/weather-in-city/index.html"
+  },
+  {
+    "revision": "a5a03d48053ef2f0e3c0",
+    "url": "/weather-in-city/static/css/main.2e175afc.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/weather-in-city/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/weather-in-city/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "a5a03d48053ef2f0e3c0",
+    "url": "/weather-in-city/static/js/main.abfdb524.chunk.js"
+  },
+  {
+    "revision": "742e4b8230b86720f145",
+    "url": "/weather-in-city/static/js/runtime~main.2bbbc489.js"
+  }
+]);
